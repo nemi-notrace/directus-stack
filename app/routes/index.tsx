51621bd3d22e1cb468c89f449c9871769ba2e03d
nemi-notrace <nemi1012@h-ka.de>
2022-11-24 12:38:30 +0100

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+export default function Index() {
+  const [state, SetState] = useState()
+
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to Remix</h1>
+      <div className="bg-slate-500 w-5 h-8">Hi</div>{' '}
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
---
+++ public/build/routes/index-YEWMTBF7.js
@@ -0,0 +1,97 @@
+import {
+  __toESM,
+  require_jsx_dev_runtime,
+  require_react
+} from "/build/_shared/chunk-IE366Y5W.js";
+
+// app/routes/index.tsx
+var import_react = __toESM(require_react());
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function Index() {
+  const [state, SetState] = (0, import_react.useState)();
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", {
+    style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.4" },
+    children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", {
+        children: "Welcome to Remix"
+      }, void 0, false, {
+        fileName: "app/routes/index.tsx",
+        lineNumber: 8,
+        columnNumber: 7
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", {
+        className: "bg-slate-500 w-5 h-8",
+        children: "Hi"
+      }, void 0, false, {
+        fileName: "app/routes/index.tsx",
+        lineNumber: 9,
+        columnNumber: 7
+      }, this),
+      " ",
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("ul", {
+        children: [
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", {
+              target: "_blank",
+              href: "https://remix.run/tutorials/blog",
+              rel: "noreferrer",
+              children: "15m Quickstart Blog Tutorial"
+            }, void 0, false, {
+              fileName: "app/routes/index.tsx",
+              lineNumber: 12,
+              columnNumber: 11
+            }, this)
+          }, void 0, false, {
+            fileName: "app/routes/index.tsx",
+            lineNumber: 11,
+            columnNumber: 9
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", {
+              target: "_blank",
+              href: "https://remix.run/tutorials/jokes",
+              rel: "noreferrer",
+              children: "Deep Dive Jokes App Tutorial"
+            }, void 0, false, {
+              fileName: "app/routes/index.tsx",
+              lineNumber: 21,
+              columnNumber: 11
+            }, this)
+          }, void 0, false, {
+            fileName: "app/routes/index.tsx",
+            lineNumber: 20,
+            columnNumber: 9
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", {
+              target: "_blank",
+              href: "https://remix.run/docs",
+              rel: "noreferrer",
+              children: "Remix Docs"
+            }, void 0, false, {
+              fileName: "app/routes/index.tsx",
+              lineNumber: 30,
+              columnNumber: 11
+            }, this)
+          }, void 0, false, {
+            fileName: "app/routes/index.tsx",
+            lineNumber: 29,
+            columnNumber: 9
+          }, this)
+        ]
+      }, void 0, true, {
+        fileName: "app/routes/index.tsx",
+        lineNumber: 10,
+        columnNumber: 7
+      }, this)
+    ]
+  }, void 0, true, {
+    fileName: "app/routes/index.tsx",
+    lineNumber: 7,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/index-YEWMTBF7.js.map
